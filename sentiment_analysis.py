5180207ee81cb19de642bbeb165f99193af7bb90
DakshChhokra <chhokradaksh@gmail.com>
2019-11-14 00:10:53 -0500

    Completed nltk analysis and sentence length analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -73,6 +73,98 @@ kennedy_urls.extend(kennedy_last_ten_urls);
 kennedy_compounds = []
 reagan_compounds = []
 
+for speech in kennedy_urls:
+	curr_speech = get_sentence_array(speech)
+	kennedy_compounds.append(get_compound_of_speech(curr_speech))
+
+for speech in reagan_urls:
+	curr_speech = get_sentence_array(speech)
+	reagan_compounds.append(get_compound_of_speech(curr_speech))
+
+print("Kennedy's sentiments were: ")
+for k in kennedy_compounds:
+	if k < 0:
+		print(kennedy_urls[kennedy_compounds.index(k)])
+	print(k)
+print("Kennedy's average sentiment was ", sum(kennedy_compounds) / len(kennedy_compounds))
+print("Kennedy's median sentiment was", np.median(kennedy_compounds))
+print("Kennedy's std dev was", np.std(kennedy_compounds))
+print("max: ", max(kennedy_compounds))
+print("min: ", min(kennedy_compounds))
+
+print("Reagan's sentiments were: ")
+for r in reagan_compounds:
+	if r < 0:
+		print(reagan_urls[reagan_compounds.index(r)])
+	print(r)
+print("Reagan's average sentiment was ", sum(reagan_compounds) / len(reagan_compounds))
+print("Reagan's median sentiment was", np.median(reagan_compounds))
+print("Reagan's std dev was", np.std(reagan_compounds))
+print("max: ", max(reagan_compounds))
+print("min: ", min(reagan_compounds))
+
+speech_numbers = []
+for x in range(1, 21):
+	speech_numbers.append(x)
+
+
+
+
+
+plt.clf()
+
+# using some dummy data for this example
+xs = speech_numbers
+ys = kennedy_compounds
+
+plt.bar(xs,ys)
+
+# zip joins x and y coordinates in pairs
+for x,y in zip(xs,ys):
+
+    label = "{:.4f}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xticks(np.arange(1,21,1))
+plt.yticks(np.arange(-0.2,0.4,0.05))
+plt.xlabel("Speech Number in Corpus")
+plt.ylabel("VADER Sentiment Analysis Compound Score")
+
+plt.show()
+
+# using some dummy data for this example
+xs = speech_numbers
+ys = reagan_compounds
+
+plt.bar(xs,ys)
+
+# zip joins x and y coordinates in pairs
+for x,y in zip(xs,ys):
+
+    label = "{:.4f}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xticks(np.arange(1,21,1))
+plt.yticks(np.arange(-0.2,0.4,0.05))
+plt.xlabel("Speech Number in Corpus")
+plt.ylabel("VADER Sentiment Analysis Compoun Score")
+
+plt.show()
+
+
+
+#ANALYSIS 2
+
 k_len = []
 r_len = []
 
@@ -91,106 +183,18 @@ for speech in reagan_urls:
 	curr_speech = sentence_length_r(get_sentence_array(speech))
 
 
-# for speech in kennedy_urls:
-# 	curr_speech = get_sentence_array(speech)
-# 	kennedy_compounds.append(get_compound_of_speech(curr_speech))
-
-# for speech in reagan_urls:
-# 	curr_speech = get_sentence_array(speech)
-# 	reagan_compounds.append(get_compound_of_speech(curr_speech))
-
-# print("Kennedy's sentiments were: ")
-# for k in kennedy_compounds:
-# 	if k < 0:
-# 		print(kennedy_urls[kennedy_compounds.index(k)])
-# 	print(k)
-
-print("Kennedy's average sentiment was ", sum(k_len) / len(k_len))
-print("Kennedy's median sentiment was", np.median(k_len))
+print("Kennedy's average sentence lengths were ", sum(k_len) / len(k_len))
+print("Kennedy's median sentence length was", np.median(k_len))
 print("Kennedy's std dev was", np.std(k_len))
 print("max: ", max(k_len))
 print("min: ", min(k_len))
 
-print("Reagan's sentiments were: ")
-print("Reagan's average sentiment was ", sum(r_len) / len(r_len))
-print("Reagan's median sentiment was", np.median(r_len))
+print("Reagan's senntence lengths were: ")
+print("Reagan's average sentence length was ", sum(r_len) / len(r_len))
+print("Reagan's median sentence length was", np.median(r_len))
 print("Reagan's std dev was", np.std(r_len))
 print("max: ", max(r_len))
 print("min: ", min(r_len))
-
-speech_numbers = []
-for x in range(1, 21):
-	speech_numbers.append(x)
-
-# fig1 = plt.figure()
-# ax1 = fig1.add_axes([0,0,1,1])
-# ax1.bar(speech_numbers,kennedy_compounds)
-# plt.show()
-
-# fig2 = plt.figure()
-# ax2 = fig2.add_axes([0,0,1,1])
-# ax2.bar(speech_numbers,reagan_compounds)
-# plt.show()
-
-# plt.plot(speech_numbers, kennedy_compounds)
-# plt.grid()
-# plt.show()
-
-# plt.plot(speech_numbers, reagan_compounds)
-# plt.grid()
-# plt.show()
-
-
-
-# plt.clf()
-
-# # using some dummy data for this example
-# xs = speech_numbers
-# ys = kennedy_compounds
-
-# plt.bar(xs,ys)
-
-# # zip joins x and y coordinates in pairs
-# for x,y in zip(xs,ys):
-
-#     label = "{:.4f}".format(y)
-
-#     plt.annotate(label, # this is the text
-#                  (x,y), # this is the point to label
-#                  textcoords="offset points", # how to position the text
-#                  xytext=(0,10), # distance from text to points (x,y)
-#                  ha='center') # horizontal alignment can be left, right or center
-
-# plt.xticks(np.arange(1,21,1))
-# plt.yticks(np.arange(-0.2,0.4,0.05))
-# plt.xlabel("Speech Number in Corpus")
-# plt.ylabel("VADER Sentiment Analysis Compound Score")
-
-# plt.show()
-
-# # using some dummy data for this example
-# xs = speech_numbers
-# ys = reagan_compounds
-
-# plt.bar(xs,ys)
-
-# # zip joins x and y coordinates in pairs
-# for x,y in zip(xs,ys):
-
-#     label = "{:.4f}".format(y)
-
-#     plt.annotate(label, # this is the text
-#                  (x,y), # this is the point to label
-#                  textcoords="offset points", # how to position the text
-#                  xytext=(0,10), # distance from text to points (x,y)
-#                  ha='center') # horizontal alignment can be left, right or center
-
-# plt.xticks(np.arange(1,21,1))
-# plt.yticks(np.arange(-0.2,0.4,0.05))
-# plt.xlabel("Speech Number in Corpus")
-# plt.ylabel("VADER Sentiment Analysis Compound Score")
-
-# plt.show()
 
 
 
